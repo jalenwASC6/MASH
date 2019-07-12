6f19dceada79abcd824fe34fd65734ae18fa7c0e
jalenwASC6 <jalenw44@nycischool.org>
2019-07-12 13:40:39 -0400

    MASH Game

--- MASH.js
+++ MASH.js
@@ -2,17 +2,17 @@ let home = ["Mansion", "Apartment", "Shack", "House"];
 let car = ["Lamborghini", "Box with Wheels", "Convertible", "Bugatti"];
 let sal = ["$100,000", "$2", "$15,000", "$40,000"];
 function mash() {
-return "You will live in a " + getHome() + ", with " + getChildrenCount() + " kids, drive a " + getCar() + " and have a yearly salary of " + getSalary() + "!" 
+return "You will live in a " + getHome() + ", with " + getChildrenCount() + " kids, drive a " + getCar() + "," + " and have a yearly salary of " + getSalary() + "!" 
 }
 function getHome() {
 return home[Math.floor(Math.random()*home.length)];
 }
 home.push(process.argv[2])
 function getChildrenCount() {
-if (Math.random() < 0.5){
+if (Math.random() < 0.5) {
 return [Math.floor(Math.random() * 100 + 1)];
 }
-else{
+else {
 return (process.argv[3]);
 }
 }
